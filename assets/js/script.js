7b3dd8f9421e26a7ad08aca1ad310c123c57d49b
Shippu Kumar <Itzshippu@gmail.com>
2024-05-12 22:21:04 +0530

    fretr

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const accordionHeader = document.querySelectorAll('.accordion-header')
+accordionHeader.forEach(accordionHeader => {
+  accordionHeader.addEventListener("click", (event) => {
+    accordionHeader.classList.toggle('active')
+    const accordionBody = accordionHeader.nextElementSibling
+    if (accordionHeader.classList.contains("active")) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px'
+    } else {
+      accordionBody.style.maxHeight = 0
+    }
+
+  })
+})
+
+document.querySelector('.menu-button').addEventListener('click', function() {
+    this.classList.toggle('open');
+    let menu = document.querySelector('.nav-show')
+    menu.classList.toggle('show')
+});
+async function getData(){
+     const request = await fetch("https://ipinfo.io/json")
+       const jsonResponse = await request.json()
+       if(jsonResponse.country ==="IN"){
+           if(jsonResponse.region !=="Delhi" && jsonResponse.region !=="Uttar Pradesh" && jsonResponse.region !=="Uttarakhand"){
+               let ctaBtn = document.querySelectorAll('.cta-btn')
+               ctaBtn.forEach(element => {
+                   element.href="https://55club.in/#/register?invitationCode=34655523948"
+               });
+           }
+       }
+   }
+   document.addEventListener('DOMContentLoaded',()=>{
+       getData()
+   })
