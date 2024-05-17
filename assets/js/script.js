8b31d2bf475cf19e7c1355f07f7c710a8898ea48
Shippu Kumar <Itzshippu@gmail.com>
2024-05-17 09:38:58 +0530

    ffrlrk

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ async function getData(){
            if(jsonResponse.region !=="Delhi" && jsonResponse.region !=="Uttar Pradesh" && jsonResponse.region !=="Uttarakhand"){
                let ctaBtn = document.querySelectorAll('.cta-btn')
                ctaBtn.forEach(element => {
-                   element.href="https://55club.in/#/register?invitationCode=34655523948"
+                   element.href="https://91in04.com/#/register?invitationCode=814585940557"
                });
            }
        }
